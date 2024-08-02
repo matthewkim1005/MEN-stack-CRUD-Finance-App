a7d13cd44b978c510775e85ec2670e30f15cd9c4
matthewkim1005 <matt_hew_kim@yahoo.com>
2024-08-02 15:45:06 -0700

    Added post and show pages for adding transactions.

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -4,7 +4,13 @@ const router = express.Router();
 const User = require('../models/user.js');
 
 router.get('/', async (req, res) => {
-    res.render('views/index.ejs', { transactions: currentUser.transactions, });
+    try {
+        const currentUser = await User.findById(req.session.user._id);
+        res.render('transactions/index.ejs', { transactions: currentUser.transactions, });
+    } catch (error) {
+        console.log(error)
+        res.redirect('/')
+    }
 });
 
 router.get('/new', async (req, res) => {
@@ -23,6 +29,19 @@ router.get('/index', async (req, res) => {
 
 router.get('/calendar', async (req, res) => {
     res.render('transactions/calendar.ejs');
+});
+
+router.post('/', async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.session.user._id);
+        req.body.starred = false;
+        currentUser.transactions.push(req.body);
+        await currentUser.save();
+        res.redirect(`/users/${currentUser._id}/transactions`);
+    } catch (error) {
+        console.log(error);
+        res.redirect('/')
+    }
 });
 
 module.exports = router;
